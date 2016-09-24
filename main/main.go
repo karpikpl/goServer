3502e027c0e1a4763f515b9c669bb052b379b2a6
Piotr Karpala <pkarpala@infusion.com>
2016-09-23 23:10:37 -0400

    basic todo list

--- main/main.go
+++ main/main.go
@@ -5,12 +5,31 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
+	router.HandleFunc("/todos", todoIndex)
+	router.HandleFunc("/todos/{todoId}", todoShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Index
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func todoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func todoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoID)
 }
