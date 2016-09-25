bb70fd60f78b511fd1a740b785d261cc4fbd72bf
Piotr Karpala <pkarpala@infusion.com>
2016-09-25 12:44:06 -0400

    todos repo

--- main/handlers.go
+++ main/handlers.go
@@ -13,11 +13,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func todoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation!"},
-		Todo{Name: "Host meetup"},
-	}
-
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
--- main/todo.go
+++ main/todo.go
@@ -4,6 +4,7 @@ import "time"
 
 // Todo object for handlign todos
 type Todo struct {
+	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Completed bool      `json:"completed"`
 	Due       time.Time `json:"due"`
