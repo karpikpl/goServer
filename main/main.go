d960cfc04cc0f80736e23c8def6858f97f421364
Piotr Karpala <pkarpala@infusion.com>
2016-09-24 21:22:32 -0400

    cleanup and logger

--- main/main.go
+++ main/main.go
@@ -1,41 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
+	router := NewRouter()
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", index)
-	router.HandleFunc("/todos", todoIndex)
-	router.HandleFunc("/todos/{todoId}", todoShow)
-
+	fmt.Println("Starting server")
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-// Index
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
-func todoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation!"},
-		Todo{Name: "Host meetup"},
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-func todoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoID := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoID)
 }
